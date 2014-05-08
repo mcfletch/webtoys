b632e0924a400a180538425002685da385a5d7ea
Mike C. Fletcher <mcfletch@vrplumber.com>
2014-05-08 01:49:19 -0400

    grouped numbers up to 100 with 2-level components

--- publish/polygons/shapenums.js
+++ publish/polygons/shapenums.js
@@ -1,12 +1,8 @@
+/** @jsx React.DOM */
 var RD = React.DOM;
 var NumberDisplay = React.createClass({
-    getInitialState: function() {
-        return {
-            number:0,
-        };
-    },
     render: function() {
-        var number = this.state.number;
+        var number = this.props.number;
         var number_nodes = [1,2,3,4,5,6,7,8,9,10].map( function(i) {
             var extra = '';
             if (i <= number){
@@ -16,13 +12,34 @@ var NumberDisplay = React.createClass({
                 RD.div( { className: "number number-"+i+extra} )
             );
         });
-        return (
-            RD.div({className:"number-display",children:number_nodes})
-        );
+        return RD.div({className:"number-display",children:number_nodes});
     }
 });
+
+var HundredDisplay = React.createClass({
+    getInitialState: function() {
+        return {
+            number: 0
+        };
+    },
+    render: function() {
+        var number = this.state.number;
+        var number_nodes = [1,2,3,4,5,6,7,8,9,10].map( function(i) {
+            var extra = '';
+            if (i <= number){
+                extra += ' enabled';
+            }
+            var child = NumberDisplay( {number:number - (10*(i-1))});
+            return RD.div( {className:"number number-"+i},child);
+        });
+        return RD.div({className:"number-display hundred",children:number_nodes});
+    }
+});
+
+
+
 $(document).ready( function() {
-    var number_display = React.renderComponent( NumberDisplay({
+    var number_display = React.renderComponent( HundredDisplay({
     }), document.getElementById('number-display-holder'));
     var choice = $('.choose-number');
     choice.change( function() {
